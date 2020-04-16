1b9764fa282c03563cab1c6b9621f75efe0072fa
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-04-16 00:45:02 -0400

    Export express app from server.js

--- server.js
+++ server.js
@@ -8,18 +8,49 @@ const dev = process.env.NODE_ENV !== 'production'
 const port = parseInt(process.env.PORT, 10) || 3000
 const distDir = 'dist'
 
-async function main () {
-  console.log('Starting server...');
-  const {middlewares} = await keystone.prepare({apps, dev, distDir});
-  await keystone.connect();
-  const app = express();
-  configureExpress(app);
-  app.use(middlewares);
-  await promisify(app.listen.bind(app))(port);
-  console.log(`Listening at http://localhost:${port}`);
+function combineMiddlewares(middlewares) {
+  let result = middlewares[0]
+  middlewares.slice(1).forEach(b => {
+    const a = result
+    result = (req, res, next) => {
+      a(req, res, error => {
+        if (error) {
+          return next(err)
+        }
+        b(req, res, next)
+      })
+    }
+  })
+  return result
 }
 
-main().catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+function crashIfThrows (promise) {
+  return promise.catch(error => {
+    console.error(error);
+    process.exit(1);
+  })
+}
+
+console.log('Starting server...');
+
+const middlewarePromise = (async () => {
+  const {middlewares} = await keystone.prepare({apps, dev, distDir});
+  await keystone.connect();
+  return combineMiddlewares(middlewares)
+})()
+
+const app = express();
+app.use(async (req, res, next) => {
+  const middleware = await middlewarePromise
+  middleware(req, res, next)
+})
+configureExpress(app);
+
+module.exports = app
+
+if (require.main === module) {
+  crashIfThrows((async () => {
+    await promisify(app.listen.bind(app))(port)
+    console.log(`Listening at http://localhost:${port}`);
+  })());
+}
